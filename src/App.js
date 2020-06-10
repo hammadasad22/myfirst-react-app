c7e44bcfac604ab6a2c03b6037ea888276cd8660
hammadasad22 <hammadasad190@gmail.com>
2020-06-10 13:12:12 +0500

    updated display message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Hello React. I have started learnign you!</h1>
     </div>
   );
 }
