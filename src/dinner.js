f31be24670c62a50e04e86e1921b0d380db8415d
hammadasad22 <hammadasad190@gmail.com>
2020-06-11 14:03:41 +0500

    updated menu

--- src/dinner.js
+++ src/dinner.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Dinner(props) {
     return (
         <div>
-            <h1>Today we are serving {props.starter} in starter</h1>
-            <h1>Today we are serving {props.mainCourse} in main course</h1>
-            <h1>Today we are serving {props.dessert} in dessert </h1>
+            <h1>Tomorrow we were serving {props.starter} in starter</h1>
+            <h1>Tomorrow we were serving {props.mainCourse} in main course</h1>
+            <h1>Tomorrow we were serving {props.dessert} in dessert </h1>
         </div>
     )
 }
